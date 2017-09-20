bab2d038f525a12a45e11a6daa19ae818f090124
Jaidon Preston <jaidonisbatman@gmail.com>
2017-09-20 13:38:45 -0700

    adding prototype testing script for TaskViewer

---
+++ Development Versions/currentDev/WallpaperChangerV7.4dev/tasktest.ps1
@@ -0,0 +1,10 @@
+﻿#While($TRUE) {ps | sort -des cpu | select -f 30 | ft -a; sleep 2; cls}
+$saveY = [console]::CursorTop
+$saveX = [console]::CursorLeft      
+
+cls
+while ($true) {
+    Get-Process | Sort -Descending -Unique SI,ProcessName| Select -First 30 -Property Username;
+    Sleep -Seconds 2;
+    [console]::setcursorposition($saveX,$saveY-6)
+}
